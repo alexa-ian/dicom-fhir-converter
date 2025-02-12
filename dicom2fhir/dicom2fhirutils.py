20920367e4b1c1ac321d4c54ba123cae85e8959d
Iancu <iancuaa@medads.uk-erlangen.de>
2025-02-12 16:26:43 +0100

    fix DateTime

--- dicom2fhir/dicom2fhirutils.py
+++ dicom2fhir/dicom2fhirutils.py
@@ -190,7 +190,7 @@ def gen_started_datetime(dt, tm):
     dt_date = datetime.strptime(dt_string, dt_pattern)
 
     # strangely, providing the datetime.date object does not work
-    fhirDtm = fhirtypes.DateTimeType(
+    fhirDtm = fhirtypes.DateTime(
         dt_date.year,
         dt_date.month,
         dt_date.day,
